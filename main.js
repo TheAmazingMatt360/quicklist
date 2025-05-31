86305a3a7ed40109f1c762117e581f9daf876617
brawlstarsdude56 <brawlstarsdude56@gmail.com>
2025-05-31 20:29:13 +0000

    criacao do main.js e criacao da funcao de adicionar items

---
+++ main.js
@@ -0,0 +1,17 @@
+const itens = [];
+
+function addItem()
+{
+    const itemName = document.querySelector("#item").value
+
+    const item =
+    {
+        name: itemName,
+        checked: false
+    }
+
+    itens.push(item)
+
+    document.querySelector("#item").value = ""
+    
+}
