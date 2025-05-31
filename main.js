c4f74b22772d54f097f5117a49b13f9c30536e45
brawlstarsdude56 <brawlstarsdude56@gmail.com>
2025-05-31 20:50:09 +0000

    criacao da funcao showitens

--- main.js
+++ main.js
@@ -13,5 +13,33 @@ function addItem()
     itens.push(item)
 
     document.querySelector("#item").value = ""
+
+    showItensList()
     
+}
+
+function showItensList()
+{
+    const list = document.querySelector(".list")
+    selectionList.textContent = ""
+
+    itens.map((item,index) =>
+    {
+        selectionList.InnerHTML += `
+        
+                    <div class="item">
+                <div>
+                    <input type="checkbox" name="list" id="item-${index}">
+
+                    <div class="custom-checkbox">
+                        <img src="./assets/checked.svg" alt="checked">
+                    </div>
+
+                    <label for="item-${index}">${item.name}</label>
+                </div>
+                <button><img src="assets/trash-icon.svg" alt=""></button>
+            </div>
+
+        `
+    })
 }
